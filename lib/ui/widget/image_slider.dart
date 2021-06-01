f08920ea6ccb0e82ed1e797e5cdf1efed40b259f
ryo <julian.aryo1989@gmail.com>
2021-06-01 22:40:22 +0800

    using set state in image slider widget

--- lib/ui/widget/image_slider.dart
+++ lib/ui/widget/image_slider.dart
@@ -1,9 +1,7 @@
-import 'package:ayov2/getx/getx.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 
-class ImageSlider extends StatelessWidget {
+class ImageSlider extends StatefulWidget {
   final int itemCount;
   final Function itemBuilder;
 
@@ -13,82 +11,80 @@ class ImageSlider extends StatelessWidget {
   });
 
   @override
+  _ImageSliderState createState() => _ImageSliderState();
+}
+
+class _ImageSliderState extends State<ImageSlider> {
+  int active = 0;
+
+  @override
   Widget build(BuildContext context) {
-    return GetBuilder(
-      init: ImageSliderController(),
-      builder: (ImageSliderController controller) {
-        return Stack(
-          children: <Widget>[
-            CarouselSlider.builder(
-              itemCount: itemCount,
-              itemBuilder: itemBuilder,
-              options: CarouselOptions(
-                viewportFraction: 1.0,
-                height: double.infinity,
-                autoPlay: true,
-                enableInfiniteScroll: true,
-                onPageChanged: (index, _) {
-                  controller.activeSlide(index);
+    return Stack(
+      children: <Widget>[
+        CarouselSlider.builder(
+          itemCount: widget.itemCount,
+          itemBuilder: widget.itemBuilder,
+          options: CarouselOptions(
+            viewportFraction: 1.0,
+            height: double.infinity,
+            autoPlay: true,
+            enableInfiniteScroll: true,
+            onPageChanged: (index, _) {
+              setState(() {
+                active = index;
+              });
+            },
+          ),
+        ),
+        Align(
+          alignment: Alignment.bottomLeft,
+          child: Padding(
+            padding: EdgeInsets.only(left: 20),
+            child: Row(
+              children: List<Widget>.generate(
+                widget.itemCount,
+                (index) {
+                  return Container(
+                    width: 8.0,
+                    height: 8.0,
+                    margin:
+                        EdgeInsets.symmetric(vertical: 10.0, horizontal: 2.0),
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: (index == active) ? Colors.red : Colors.grey[100],
+                    ),
+                  );
                 },
               ),
             ),
-            Align(
-              alignment: Alignment.bottomLeft,
-              child: Padding(
-                padding: EdgeInsets.only(left: 20),
-                child: Obx(
-                  () {
-                    return Row(
-                      children: List<Widget>.generate(
-                        itemCount,
-                        (index) {
-                          return Container(
-                            width: 8.0,
-                            height: 8.0,
-                            margin: EdgeInsets.symmetric(
-                                vertical: 10.0, horizontal: 2.0),
-                            decoration: BoxDecoration(
-                              shape: BoxShape.circle,
-                              color: (index == controller.activeSlide.value)
-                                  ? Colors.red
-                                  : Colors.grey[100],
-                            ),
-                          );
-                        },
-                      ),
-                    );
-                  },
-                ),
-              ),
-            ),
-            // Align(
-            //   alignment: Alignment.bottomRight,
-            //   child: GestureDetector(
-            //     onTap: () {},
-            //     child: Container(
-            //       height: 20,
-            //       width: 100,
-            //       alignment: Alignment.center,
-            //       decoration: BoxDecoration(
-            //         color: Colors.red.withOpacity(0.8),
-            //         borderRadius: BorderRadius.only(
-            //           topLeft: Radius.circular(15),
-            //         ),
-            //       ),
-            //       child: Text(
-            //         'Lihat Semua',
-            //         textAlign: TextAlign.right,
-            //         style: TextStyle(
-            //           color: Colors.white,
-            //           fontSize: 10,
-            //         ),
-            //       ),
-            //     ),
-            //   ),
-            // ),
-          ],
-        );
-      },
+          ),
+        ),
+        // Align(
+        //   alignment: Alignment.bottomRight,
+        //   child: GestureDetector(
+        //     onTap: () {},
+        //     child: Container(
+        //       height: 20,
+        //       width: 100,
+        //       alignment: Alignment.center,
+        //       decoration: BoxDecoration(
+        //         color: Colors.red.withOpacity(0.8),
+        //         borderRadius: BorderRadius.only(
+        //           topLeft: Radius.circular(15),
+        //         ),
+        //       ),
+        //       child: Text(
+        //         'Lihat Semua',
+        //         textAlign: TextAlign.right,
+        //         style: TextStyle(
+        //           color: Colors.white,
+        //           fontSize: 10,
+        //         ),
+        //       ),
+        //     ),
+        //   ),
+        // ),
+      ],
     );
   }
 }
